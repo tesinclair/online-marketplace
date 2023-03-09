0c2b337ed3113233cb77a8a61758debddb27357d
tesinclair <blykrise@gmail.com>
2023-03-09 15:59:37 +0100

    breaking changes in nodemodules, adding code changes then origin

--- Frontend/frontend/src/components/NavBar.js
+++ Frontend/frontend/src/components/NavBar.js
@@ -1,17 +1,22 @@
-function NavBar(){
+function NavBar(props){
   return(
-    <nav>
-      <a href="/">Online MarketPlace</a>
-      <SearchBar></SearchBar>
-      <ul>
-        <li><a href="/login">Login</a> {/* only for unlogged in customers */}</li>
-        <li><a href="/create-account">Create Account</a> {/* only for unlogged in customers */}</li>
-      </ul>
-      <a href="/checkout">Checkout</a>
-      <ul>
-        <li><a href="/order-track">Track your orders</a> {/* Only for logged in customers */}</li>
-        <li>Log Out</li>
-      </ul>
+    <nav className = "nav">
+      <a href="/">Online Marketplace</a>
+      {/*<SearchBar></SearchBar>*/}
+      {
+      props.loggedIn?
+        <ul className = "options">
+          <li><a href="/order-track">Track your orders</a> {/* Only for logged in customers */}</li>
+          <li>Log Out</li>
+        </ul>
+        :
+        <ul className = "options">
+          <li><a href="/login">Login</a> {/* only for unlogged in customers */}</li>
+          <li><a href="/create-account">Create Account</a> {/* only for unlogged in customers */}</li>
+        </ul>
+      }
+      <a className = "options" href="/checkout">Checkout</a>
+      
     </nav>
   );
 }
