4bf2ede76c8b75dee600963bebd3544be681afd4
tesinclair <blykrise@gmail.com>
2023-03-09 16:30:55 +0100

    Added react router to package-lock, and App.js, renamed components to comply with react nameing standards

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from './pages/MainPage.js';
+import Checkout from './pages/Checkout.js';
+import Create from './pages/Create.js';
+import Login from './pages/Login.js';
+import OrderTracking from './pages/OrderTracking';
 import NavBar from './components/NavBar.js';
 
 function App() {
