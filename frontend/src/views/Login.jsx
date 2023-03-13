bda1ac1d0c11e7d6aa868bf751f17caae861fbc9
tesinclair <blykrise@gmail.com>
2023-03-13 18:55:56 +0100

    added email and password form textfields

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,6 +1,34 @@
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
 function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <h1>login</h1>
+    <div>
+       <h1>login</h1>
+       <div className = "form">
+        <TextField
+          id="outlined-controlled"
+          type="email"
+          label="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }} 
+        />
+        <TextField 
+          id="outlined-controlled"
+          type="password"
+          label="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+       </div>
+    </div>
+
   );
 }
 export default Login;
